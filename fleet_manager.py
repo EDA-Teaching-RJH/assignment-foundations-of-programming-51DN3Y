d8c231755e40e316a6940d04ecb358518583d0a4
Sidney Beacher <163619322+51DN3Y@users.noreply.github.com>
2026-02-23 01:19:36 +0000

    Added Docstrings and Comments

--- fleet_manager.py
+++ fleet_manager.py
@@ -1,4 +1,5 @@
 def main():
+    '''Main function to run the fleet manager program. Displays menu and prompts user for input until they choose to exit.'''
     print("\nWelcome to Fleet Manager System")
     name, rank, division, id, valid_rank, student_name = init_database() 
 
@@ -28,6 +29,7 @@ def main():
 
 
 def init_database():
+    '''Initialises the database with default crew members and prompts the user for their name. Returns the lists for names, ranks, divisions, IDs, valid ranks, and the student name.'''
     print("Initializing database...")
     student_name = input("\nEnter Full Name: ")
     print(f"\nWelcome, {student_name.upper()}! \nLoading menu...",)
@@ -42,6 +44,7 @@ def init_database():
     
 
 def display_menu():
+    '''Displays the menu options to the user and prompts for input. Returns the selected option.'''
     print("\n--- MENU ---")
     print("1. Add Crew")
     print("2. Remove Crew")
@@ -61,6 +64,7 @@ def display_menu():
     return opt
 
 def add_member(name, rank, division, id, valid_rank):
+    '''Add a crew member to the lists. User must enter a unique ID and a valid rank to add the crew member to the lists.'''
     new_name = input("Name: ")
     new_rank = input("Rank: ")
     new_div = input("Division: ")
@@ -85,11 +89,12 @@ def add_member(name, rank, division, id, valid_rank):
 
 
 def remove_member(name, rank, division, id):
+    '''Remove a crew member by their ID. User must enter a valid ID to remove the corresponding crew member from all lists. Uses index search to find the index of the crew member in the lists, then uses pop to remove the crew member from all lists.'''
     remove = int(input("Enter ID of crew member to remove: "))
 
     if remove in id:
-        idx = id.index(remove)
-        name.pop(idx)
+        idx = id.index(remove) 
+        name.pop(idx) 
         rank.pop(idx)
         division.pop(idx)
         id.pop(idx)
@@ -99,39 +104,43 @@ def remove_member(name, rank, division, id):
 
 
 def update_rank(rank, id):
+    '''Update the rank of a crew member by their ID. User must enter a valid ID and a new rank.'''
     update = int(input("Enter ID of crew member to update rank: "))
 
-    while update not in id:
+    while update not in id: # If the ID entered by the user is not found in the id list, it will prompt the user to enter a valid ID until a valid ID is entered.
         print("ID not found. Please enter a valid ID.")
         update = int(input("Enter ID of crew member to update rank: "))
         
     idx = id.index(update)
     new_rank = input("New Rank: ")
-    rank[idx] = new_rank
+    rank[idx] = new_rank # Update the rank of the crew member at the index found by the ID search with the new rank entered by the user.
     print("Rank updated.")
 
 
 def display_roster(name, rank, division, id):
+    '''Displaying roster in a table like format with headers and spacing'''
     print("NAME                 RANK                      DIVISION        ID")
     print("-------------------------------------------------------------------------")
     
     for i in range(len(name)):
-        print(f"{name[i]:20} {rank[i]:25} {division[i]:15} {id[i]}")
+        print(f"{name[i]:20} {rank[i]:25} {division[i]:15} {id[i]}") # Using spacing e.g :25, to have 25 spaces to align data for table like format
 
 
 def search_crew(name, rank, division, id):
+    '''Search for crew members by name, matches any input with a name in the list.'''
     search = input("Enter name to search: \n").lower()
     found = False
 
     for i in range(len(name)):
         if search in name[i].lower():
-            print(f"{name[i]}, {rank[i]}, {division[i]}, ID: {id[i]}")
+            print(f"{name[i]}, {rank[i]}, {division[i]}, ID: {id[i]}") # Displaying formatted details of each crew member found in search
             found = True
     if not found:
         print("Crew member not found.")
 
 
 def filter_by_division(name, division):
+    '''Filter crew members by division. User must enter Command, Operations, or Sciences.'''
     search = input("Enter division (Command, Operations, or Sciences): ")
 
     while search not in ["Command", "Operations", "Sciences"]:
@@ -139,7 +148,7 @@ def filter_by_division(name, division):
         search = input("Enter division (Command, Operations, or Sciences): ")
     
     found = False
-    for i in range(len(division)):
+    for i in range(len(division)): # Loops through division list to find matches with user input, if match found, display name and division of crew member
         if division[i] == search:
             print(f"Name: {name[i]}, {division[i]}")
             found = True
@@ -148,6 +157,7 @@ def filter_by_division(name, division):
 
 
 def calculate_payroll(rank):
+    '''Counts number of each rank and multiplies by salary for each rank, then adds together for total payroll.'''
     value = 0
 
     for r in rank:
@@ -171,6 +181,7 @@ def calculate_payroll(rank):
 
 
 def count_officers(rank):
+    '''Counts and displays number of Commanders and Captains in the crew list.'''
     count = 0
     for r in rank:
         if r == "Commander" or r == "Captain":
